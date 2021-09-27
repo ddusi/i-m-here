5dfe17cc510605accb1b006c72dca44077e65fa8
nokkcha <hover16@naver.com>
2021-09-27 22:59:40 +0900

    Updata :: 모바일 메타데이터 수정

--- im-here-react/src/App.js
+++ im-here-react/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import Routes from "./Routes";
-
+import { Helmet } from "react-helmet";
+import here from "./assets/mobile-here.png";
 function App() {
+  const currentUrl = document.location.href;
   return (
     <div className="App">
+      <Helmet>
+        <meta property="og:url" content={currentUrl}></meta>
+        <meta property="og:title" content="Here - 내 위치 공유하기" />
+        <meta
+          property="og:description"
+          content="쉬운 실시간 위치 공유 서비스 Here"
+        />
+        <meta property="og:image" content={here} />
+        <title>HERE - 나 여기야</title>
+        {/* <link rel="icon" href={}/> */}
+      </Helmet>
       <Routes />
     </div>
   );
